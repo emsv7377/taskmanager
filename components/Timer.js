a15fb09e3e920e012a318d902ae322c4459ef350
mnordness <michaela.nordness.6056@student.uu.se>
2023-12-18 11:36:50 +0100

    Timer is now working, and sound should be playing
    but the sound is not audible on the device

--- components/Timer.js
+++ components/Timer.js
@@ -1,60 +1,132 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import Torch from 'react-native-torch';
+import { Audio } from 'expo-av';
+
+import * as Permissions from 'expo-permissions';
+
+import ThemeContext from '../components/ThemeContext';
+import Styles from './Styles';
 
 const Timer = () => {
-  const [timerSeconds, setTimerSeconds] = useState(10); // Set your desired timer duration in seconds
+  const initialTimerSeconds = 3;
+  const [timerSeconds, setTimerSeconds] = useState(initialTimerSeconds);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
+
+  const [sound, setSound] = useState();
+  const [isSoundLoaded, setIsSoundLoaded] = useState(false);
+
+  const requestAudioPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    if (status !== 'granted') {
+      console.warn('Audio permission not granted');
+    }
+  };
+
+
+  const { theme } = useContext(ThemeContext);
+  const { colors: themeColors } = theme;
+  const styles = Styles({ themeColors });
+  
+  useEffect(() => {
+    requestAudioPermission();
+    const loadSound = async () => {
+      try {
+        console.log('Before load');
+        const { sound: audioSound } = await Audio.Sound.createAsync(
+          //require('./alarm-clock-ringing.mp3')
+          require('./alarm-piano.wav')
+        );
+        console.log('Sound loaded successfully');
+        setSound(audioSound);
+        setIsSoundLoaded(true);
+      } catch (error) {
+        console.error('Failed to load the sound', error);
+      }
+    };
+  
+    loadSound();
+  
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+    }, []);
 
   useEffect(() => {
     let timerInterval;
 
-    if (isTimerRunning) {
+    if (isTimerRunning && timerSeconds > 0) {
       timerInterval = setInterval(() => {
         setTimerSeconds((prevSeconds) => {
-          if (prevSeconds === 0) {
-            clearInterval(timerInterval);
-            handleTimerCompletion();
-            return 0;
-          }
           return prevSeconds - 1;
         });
       }, 1000);
+    } else if (timerSeconds === 0) {
+      handleTimerCompletion();
     }
 
     return () => {
       clearInterval(timerInterval);
     };
-  }, [isTimerRunning]);
+  }, [isTimerRunning, timerSeconds]);
 
   const handleTimerStart = () => {
     setIsTimerRunning(true);
   };
 
-  const handleTimerCompletion = () => {
+  const handleTimerStop = () => {
     setIsTimerRunning(false);
-    Torch.switchState(true); // Turn on the flash
-    // You may want to add a delay before turning off the flash
-    setTimeout(() => {
-      Torch.switchState(false); // Turn off the flash after a delay
-    }, 2000);
-    // Add other actions here
   };
 
   const handleTimerReset = () => {
     setIsTimerRunning(false);
-    setTimerSeconds(10); // Reset the timer duration
+    setTimerSeconds(initialTimerSeconds);
     Torch.switchState(false); // Make sure the flash is off
+  };
+
+  const handleTimerCompletion = async () => {
+    setIsTimerRunning(false);
+
+    // Logic for flashing the torch 
+    /*if (Torch && Torch.hasTorch) {
+      Torch.switchState(true); // Turn on the flash
+      // You may want to add a delay before turning off the flash
+      setTimeout(() => {
+        Torch.switchState(false); // Turn off the flash after a delay
+      }, 2000);
+    } else {
+      console.warn('Device does not support Torch/Flashlight');
+    }*/
+    // Play the sound 
+    if (isSoundLoaded && sound && !sound.isPlaying) {
+        try {
+          await sound.setPositionAsync(0);
+          await sound.setVolumeAsync(1.0);
+          await sound.playAsync();
+          console.warn('Successfully playing a sound');
+        } catch (error) {
+          console.error('Failed to play the sound', error);
+        }
+      } else {
+        console.warn('Sound is not loaded or is already playing');
+      }
+    // Add other actions here
   };
 
   return (
     <View>
-      <Text>Timer: {timerSeconds}s</Text>
-      <TouchableOpacity 
-        style={{ color:'white', padding:10, }}
-        onPress={handleTimerStart}>
-        <Text>Start Timer</Text>
-      </TouchableOpacity>
+      <Text style={styles.text}>Timer: {timerSeconds} s</Text>
+      {!isTimerRunning ? (
+        <TouchableOpacity style={styles.button} onPress={handleTimerStart}>
+          <Text>Start Timer</Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity style={styles.button} onPress={handleTimerStop}>
+          <Text>Stop Timer</Text>
+        </TouchableOpacity>
+      )}
       <TouchableOpacity onPress={handleTimerReset}>
         <Text>Reset Timer</Text>
       </TouchableOpacity>
